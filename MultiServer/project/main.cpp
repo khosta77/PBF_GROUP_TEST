88d75061d1b7bb84d898cbd6564521af2d96f11c
khosta77 <puwerfulpants@gmail.com>
2024-09-17 12:25:18 +0300

    Код сервера

--- MultiServer/project/main.cpp
+++ MultiServer/project/main.cpp
@@ -12,6 +12,8 @@
 #include <chrono>
 #include <exception>
 #include <string>
+#include <ctime>
+#include <sstream>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -39,17 +41,78 @@ public:
     ServerException( const std::string& m ) : MyException(m) {}
 };
 
+struct DateTime {
+    std::chrono::system_clock::time_point time_point;
+
+    DateTime() : time_point(std::chrono::system_clock::now()) {}
+
+    std::string to_string() const {
+        auto time_t = std::chrono::system_clock::to_time_t(time_point);
+        auto local_tm = *std::localtime(&time_t);
+        auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(time_point.time_since_epoch()).count() % 1000;
+
+        std::ostringstream oss;
+        oss << std::put_time(&local_tm, "[%Y-%m-%d %H:%M:%S.") << std::setfill('0') << std::setw(3) << milliseconds << "]";
+        return oss.str();
+    }
+
+    int get_seconds() const {
+        return std::chrono::duration_cast<std::chrono::seconds>(time_point.time_since_epoch()).count() % 60;
+    }
+
+    int get_minutes() const {
+        return std::chrono::duration_cast<std::chrono::minutes>(time_point.time_since_epoch()).count() % 60;
+    }
+
+    int get_hours() const {
+        return std::chrono::duration_cast<std::chrono::hours>(time_point.time_since_epoch()).count() % 24;
+    }
+
+    int get_year() const {
+        auto time_t = std::chrono::system_clock::to_time_t(time_point);
+        auto local_tm = *std::localtime(&time_t);
+        return local_tm.tm_year + 1900;
+    }
+
+    int get_month() const {
+        auto time_t = std::chrono::system_clock::to_time_t(time_point);
+        auto local_tm = *std::localtime(&time_t);
+        return local_tm.tm_mon + 1;
+    }
+
+    int get_day() const {
+        auto time_t = std::chrono::system_clock::to_time_t(time_point);
+        auto local_tm = *std::localtime(&time_t);
+        return local_tm.tm_mday;
+    }
+};
+
+std::vector<std::string> split( const std::string& content, const char separator )
+{
+    std::vector<std::string> arrayString;
+    std::string buffer = "";
+    for( size_t i = 0, I = content.size(); i < I; ++i )
+    {
+        if( content[i] != separator )
+            buffer += content[i];
+        else
+        {
+            arrayString.push_back(buffer);
+            buffer = "";
+        }
+    }
+    arrayString.push_back(buffer);
+    buffer.clear();
+    return arrayString;
+}
+
 class Server final
 {
 private:
     int _listener;
     struct sockaddr_in _addr;
 
-    std::mutex _m_save;
-    std::mutex _m_vector;
     std::vector<int> _socketsToClear;
-    
-    std::vector<std::thread> _threadPool;
 
     std::set<int> _clients;
     fd_set _readset;
@@ -69,7 +132,7 @@ private:
                 return "";
             }
             receivedData.append( buffer, bytesReceived );
-            if( receivedData.back() == ';' )
+            if( receivedData.back() == '\n' )
                 break;
         }
         delete []buffer;
@@ -90,96 +153,11 @@ private:
         }
     }
 
-    void updateClientsInfo()
-    {
-        // Заполняем множество сокетов
-        FD_ZERO( &_readset );
-        FD_SET( _listener, &_readset );
-        for( auto it = _clients.begin(); it != _clients.end(); it++ )
-            FD_SET( *it, &_readset );
-    }
-
-    // Ждём события в одном из сокетов
-    void waitEvent()
-    {
-        int mx = std::max( _listener, *max_element( _clients.begin(), _clients.end() ) );
-        int sel = select( ( mx + 1 ), &_readset, NULL, NULL, &_timeout );
-
-        if( sel < 0 )
-            throw ServerException( ( "select " + std::to_string(sel) ) );
-        else if( sel == 0 )
-        {
-            std::cout << "select return 0" << std::endl;
-            if( !_threadPool.empty() )
-                _threadPool.clear();
-        }
-    }
-
-    void fillSockNew()
-    {
-        if( FD_ISSET( _listener, &_readset ) )
-        {
-            if( int sock = accept( _listener, NULL, NULL ); sock >= 0 )
-            {
-                fcntl( sock, F_SETFL, O_NONBLOCK );
-                _clients.insert(sock);
-                std::cout << "new socket init: " << sock << std::endl;
-            }
-            else
-                throw ServerException( "Sock no accept" );
-        }
-    }
-
     void clearSocket( const int& sock )
     {
         std::cout << "socket " << sock << " free" << std::endl;
         close(sock);
-        
-        std::lock_guard<std::mutex> mtx1_lock(_m_save);
-        {
-            std::lock_guard<std::mutex> mtx2_lock(_m_vector);
-            {
-                _socketsToClear.push_back(sock);
-            }
-        }
-    }
-
-    void process( const int& sock )
-    {
-        std::string content = readFromRecv( sock );
-        if( content.empty() )
-            clearSocket( sock );
-        else
-            sendToSock( sock, ( std::to_string(i++) + "|:" + content ) );
-    }
-
-    // Определяем тип события и выполняем соответствующие действия
-    void launchProcess()
-    {
-        for( auto it = _clients.begin(); it != _clients.end(); it++ )
-        {
-            if( FD_ISSET( *it, &_readset ) )
-                _threadPool.emplace_back( &Server::process, this, *it );
-        }
-
-        for( auto& it : _threadPool )  // Ждем потоки
-        {   
-            if( it.joinable() )
-                it.join();
-        }
-    }
-
-    void clearUnusedSockets()
-    {
-        std::lock_guard<std::mutex> mtx1_lock(_m_save);
-        {
-            std::lock_guard<std::mutex> mtx2_lock(_m_vector);
-            {
-                for( const auto& it : _socketsToClear )
-                    _clients.erase(it);
-                _socketsToClear.clear();
-            }
-        }
+        _socketsToClear.push_back(sock);
     }
 
 public:
@@ -201,33 +179,91 @@ public:
         std::cout << "server in system address: " << IP << ":" << PORT << std::endl;
         
         // Задаём таймаут
-        _timeout.tv_sec = 15;
+        _timeout.tv_sec = 1;
         _timeout.tv_usec = 0;
     }
 
     ~Server()
     {
         _clients.clear();
-        std::lock_guard<std::mutex> mtx1_lock(_m_save);
-        {
-            std::lock_guard<std::mutex> mtx2_lock(_m_vector);
-            {
-                _socketsToClear.clear();
-            }
-        }
+        _socketsToClear.clear();
     }
+     
+    struct ClientData
+    {
+        std::string _name;
+        int _t;
 
+        ClientData( const std::string& name = "", const int& T = -1 ) : _name(name), _t(T) {}
+        ClientData& operator=( const ClientData& rhs )
+        {
+            if( this->_name != rhs._name )
+            {
+                this->_name = rhs._name;
+                this->_t = rhs._t;
+            }
+            return *this;
+        }
+    };
 
-
+    std::map<int, ClientData> df;
+    
     int run()
     {
         while( true )
         {
-            updateClientsInfo();
-            waitEvent();
-            fillSockNew();
-            launchProcess();
-            clearUnusedSockets();
+            // Заполняем множество сокетов
+            FD_ZERO( &_readset );
+            FD_SET( _listener, &_readset );
+            for( auto it = _clients.begin(); it != _clients.end(); it++ )
+                FD_SET( *it, &_readset );
+            
+            int mx = std::max( _listener, *max_element( _clients.begin(), _clients.end() ) );
+            int sel = select( ( mx + 1 ), &_readset, NULL, NULL, &_timeout );
+
+            if( sel < 0 )
+                throw ServerException( ( "select " + std::to_string(sel) ) );
+            else if( sel == 0 ) {}
+            
+            if( FD_ISSET( _listener, &_readset ) )
+            {
+                if( int sock = accept( _listener, NULL, NULL ); sock >= 0 )
+                {
+                    fcntl( sock, F_SETFL, O_NONBLOCK );
+                    _clients.insert(sock);
+                }
+                else
+                    throw ServerException( "Sock no accept" );
+            }
+            
+            for( auto it = _clients.begin(); it != _clients.end(); it++ )
+            {
+                if( FD_ISSET( *it, &_readset ) )  // Считать данные о том если новые данные о потоке
+                {
+                    std::string content = readFromRecv( *it );
+                    if( content.empty() )
+                        clearSocket( *it );
+                    else
+                    {
+                        content.pop_back();
+                        auto vec = split( content, ' ' );
+                        df[*it] = ClientData( vec[0], std::stoi(vec[1]) );
+                    }
+                }
+            }
+
+            for( auto it = df.begin(); it != df.end(); ++it )
+            {
+                DateTime datetime;
+                if( ( ( datetime.get_seconds() % it->second._t ) == 0 ) )
+                    std::cout << datetime.to_string() << ' ' << it->second._name << std::endl;
+            }
+
+            for( const auto& it : _socketsToClear )
+            {
+                _clients.erase(it);
+            }
+            _socketsToClear.clear();
         }
         return 0;
     }
